0310bccda753306c9e1e8853182bea13e084e70f
liuyangstudio <1092953332@qq.com>
2017-06-16 20:28:49 +0800

    //

---
+++ 1.php
@@ -0,0 +1,8 @@
+<?php
+echo date('Y-i.s,H');
+/**
+ * Created by PhpStorm.
+ * User: 刘洋
+ * Date: 2017/6/16
+ * Time: 16:54
+ */
--- 2.php
+++ 2.php
@@ -1,5 +1,10 @@
 <?php
-
+$year =& $_POST['year'];
+if ($year % 4 == 0 && $year % 100 !== 0 || $year % 400 == 0) {
+    echo $year . '是闰年';
+} else {
+    echo $year . '不是闰年';
+}
 
 
 ?>
@@ -10,6 +15,15 @@
     <title>Title</title>
 </head>
 <body>
+
+<form action="2.php" method="post">
+
+    <input type="text" name="year" placeholder="输入一个年份" maxlength="4" minlength="4" size="4">
+    <input type="submit" value="提交">
+
+
+</form>
+
 
 </body>
 </html>
