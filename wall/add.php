7cb9f999e0b1120c8f4cef66d006b63e2ca99a56
liuyangstudio <1092953332@qq.com>
2017-06-22 12:13:31 +0800

    //

--- wall/add.php
+++ wall/add.php
@@ -6,7 +6,7 @@ $color = $_POST['idvalue'];
 
 $connect = mysqli_connect('localhost', 'root', 84708597, 'wall');
 if (mysqli_query($connect, 'insert wall (name,content,color) values("' . $name . '","' . $content . '","' . $color . '")')) {
-    echo '<script> alert("添加成功！！！")</script>';
+    echo '<script> alert("添加成功！！！");window.location.href="index.php";</script>';
 } else {
     echo '<script >alert("失败并返回");window.location.href="index.php";</script>';
 }
---
+++ wall/delete.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘洋
+ * Date: 2017/6/22
+ * Time: 12:13
+ */
