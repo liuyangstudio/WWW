293b74df45a5575ec458611be52f2b3675228090
liuyangstudio <1092953332@qq.com>
2017-06-22 10:57:39 +0800

    //

---
+++ wall/add.php
@@ -0,0 +1,12 @@
+<?php
+
+
+if ($_POST['submit']) {
+    $name = $_POST['name'];
+$content=$_POST['content'];
+$color=$_POST['idvalue'];
+
+
+echo $name,$content,$color;
+
+}
---
+++ wall/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>许愿墙</title>
+	<link rel="stylesheet" href="./Css/index.css" />
+	<script type="text/javascript" src='./Js/jquery-1.7.2.min.js'></script>
+	<script type="text/javascript" src='./Js/index.js'></script>
+</head>
+<body>
+	<div id='top'>
+		<span id='send'></span>
+	</div>
+	<div id='main'>
+		<dl class='paper a1'>
+			<dt>
+				<span class='username'>慕课网</span>
+				<span class='num'>No.00001</span>
+			</dt>
+			<dd class='content'>大家要好好学习PHP语言哦！</dd>
+			<dd class='bottom'>
+				<span class='time'>今天08:30</span>
+				<a href="" class='close'></a>
+			</dd>
+		</dl>
+		
+		<dl class='paper a2'>
+			<dt>
+				<span class='username'>慕课网</span>
+				<span class='num'>No.00001</span>
+			</dt>
+			<dd class='content'>大家要好好学习PHP语言哦！</dd>
+			<dd class='bottom'>
+				<span class='time'>今天08:30</span>
+				<a href="" class='close'></a>
+			</dd>
+		</dl>
+
+		<dl class='paper a3'>
+			<dt>
+				<span class='username'>慕课网</span>
+				<span class='num'>No.00001</span>
+			</dt>
+			<dd class='content'>大家要好好学习PHP语言哦！</dd>
+			<dd class='bottom'>
+				<span class='time'>今天08:30</span>
+				<a href="" class='close'></a>
+			</dd>
+		</dl>
+
+		<dl class='paper a4'>
+			<dt>
+				<span class='username'>慕课网</span>
+				<span class='num'>No.00001</span>
+			</dt>
+			<dd class='content'>大家要好好学习PHP语言哦！</dd>
+			<dd class='bottom'>
+				<span class='time'>今天08:30</span>
+				<a href="" class='close'></a>
+			</dd>
+		</dl>
+
+		<dl class='paper a5'>
+			<dt>
+				<span class='username'>慕课网</span>
+				<span class='num'>No.00001</span>
+			</dt>
+			<dd class='content'>大家要好好学习PHP语言哦！</dd>
+			<dd class='bottom'>
+				<span class='time'>今天08:30</span>
+				<a href="" class='close'></a>
+			</dd>
+		</dl>
+	
+	</div>
+	
+<!--[if IE 6]>
+    <script type="text/javascript" src="./Js/iepng.js"></script>
+    <script type="text/javascript">
+        DD_belatedPNG.fix('#send,#close,.close','background');
+    </script>
+<![endif]-->
+</body>
+</html>
